e2f5e469562bc3578d0e928d311e71114c1085bf
VladSkopenko <skopirka2k17@gmail.com>
2024-03-27 18:05:46 +0300

    make decode refresh token

--- src/services/auth.py
+++ src/services/auth.py
@@ -45,8 +45,16 @@ class Auth:
         encoded_refresh_token = jwt.encode(to_encode, self.SECRET_KEY, algorithm=self.ALGORITHM)
         return encoded_refresh_token
 
-    async def decode_refresh_token(refresh_token: str):
-        ...
+    async def decode_refresh_token(self, refresh_token: str):
+        try:
+            payload = jwt.decode(refresh_token, self.SECRET_KEY, algorithms=[self.ALGORITHM])
+            if payload['scope'] == 'refresh_token':
+                email = payload['sub']
+                return email
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid scope for token')
+        except JWTError:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate credentials')
+
 
     async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
         ...
