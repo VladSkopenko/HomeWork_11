a9bd6c7b360b969f91972902d593d1525813e72e
VladSkopenko <skopirka2k17@gmail.com>
2024-03-27 16:42:35 +0300

    finish schemas

--- src/schemas/user.py
+++ src/schemas/user.py
@@ -1,12 +1,24 @@
 from pydantic import BaseModel, Field
-from datetime import date
 
 
 class UserModel(BaseModel):
     username: str = Field(min_length=2, max_length=50)
     email: str = Field(min_length=5, max_length=250)
-    password: str = Field(min_length=2, max_length=200)
+    password: str = Field(min_length=2, max_length=10)
 
 
-class UserResponse(UserModel):
-    ...
+class UserResponse(BaseModel):
+    id: int = 1
+    username: str
+    email: str
+    avatar: str
+
+    class Config:
+        from_attributes = True
+
+
+class TokenSchema(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str = "bearer"
+
