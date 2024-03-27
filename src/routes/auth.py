f76baebb2252ff6d2810aa25959e364ff9eb1a65
VladSkopenko <skopirka2k17@gmail.com>
2024-03-27 18:21:54 +0300

    pause 1:19

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -9,8 +9,14 @@ router = APIRouter(prefix='/auth', tags=["auth"])
 
 @router.post("/signup")
 async def signup(body: UserModel, db: Session = Depends(get_db)):
-    pass
-    return {}
+    exist_user = db.query(User).filter(User.email == body.username).first()
+    if exist_user:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Account already exists")
+    new_user = User(email=body.username, password=hash_handler.get_password_hash(body.password))
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return {"new_user": new_user.email}
 
 
 @router.post("/login")
